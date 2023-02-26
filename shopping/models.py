8a495884043d880eb34b5fc13f267b5e5ac54898
stamplevskiyd <stamplevskiyd@gmail.com>
2023-02-27 02:29:12 +0300

    Добавлены аватарки пользователям, парсятся из ВК

--- shopping/models.py
+++ shopping/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+
+import random
+import requests
 from datetime import date
-from django.shortcuts import reverse
 
 
 class User(models.Model):
@@ -9,11 +11,37 @@ class User(models.Model):
     first_name = models.CharField(max_length=50, blank=False, verbose_name='Имя')
     second_name = models.CharField(max_length=50, blank=False, verbose_name='Фамилия')
     register_date = models.DateField(default=date.today(), verbose_name='Дата регистрации')
+    phone_number = models.CharField(max_length=11, verbose_name='Номер телефона')
+    vk_profile_link = models.URLField(max_length=200, blank=True, verbose_name='Профиль в ВК')
 
     @property
     def full_name(self):
         """Полное имя пользователя"""
         return ' '.join((self.first_name, self.second_name))
+
+    @property
+    def format_phone_number(self):
+        """Номер телефона в более красивом формате"""
+        return self.phone_number
+
+    @property
+    def profile_picture_url(self):
+        """Парсит страничку ВК и находит ссылку на аватарку"""
+
+        #TODO: Переделать, пока находит только картинки шакального качества
+
+        r = requests.get(self.vk_profile_link)
+        lines = r.content.split()
+
+        index = 0
+        while lines[index] != b'style="background-image:':
+            index += 1
+
+        print(str(lines[index + 1]))
+        url = str(lines[index + 1]).split(';')[1]
+        return url
+
+
 
 
 class Store(models.Model):
